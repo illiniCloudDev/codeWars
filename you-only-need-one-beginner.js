c58a49ba86c31f0dae8502b8cd831f320113d1a4
illiniCloudDev <107890959+illiniCloudDev@users.noreply.github.com>
2022-08-15 18:24:49 -0500

    uploaded another kata

---
+++ you-only-need-one-beginner.js
@@ -0,0 +1,14 @@
+// You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+
+// Array can contain numbers or strings. X can be either.
+
+// Return true if the array contains the value, false if not.
+
+//My solution below
+
+function check(arr, value) {
+    //we need a loop
+    //we need to compare array element with value
+    //a conditional if it is not true
+    return arr.includes(value) // we used the include method to determine if the variable was present in boolean
+  }
