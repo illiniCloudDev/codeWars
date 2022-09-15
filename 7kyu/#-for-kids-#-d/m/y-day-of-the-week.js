0cf04f88a1adf6b4beb32101868b04977df70201
illiniCloudDev <107890959+illiniCloudDev@users.noreply.github.com>
2022-09-14 23:07:44 -0500

    7kyu kata

---
+++ 7kyu/#-for-kids-#-d/m/y-day-of-the-week.js
@@ -0,0 +1,8 @@
+// Your task is easy, write a function that takes an date in format d/m/Y(String) and return what day of the week it was(String).
+// Example: "21/01/2017" -> "Saturday", "31/03/2017" -> "Friday"
+
+
+//My solution 
+
+const dayOfTheWeek = date =>
+  new Date (date.split('/').reverse().join('-')).toLocaleString('en-us', {weekday: 'long'})
