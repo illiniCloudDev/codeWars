c399fcb56a1fb6ce1a4fc510775e5da31b0101b1
illiniCloudDev <107890959+illiniCloudDev@users.noreply.github.com>
2022-11-02 14:19:11 -0500

    6kyu kata. Review.

---
+++ 6kyu/persistent-bugger.js
@@ -0,0 +1,24 @@
+// Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence, which is the number of times you must multiply the digits in num until you reach a single digit.
+
+// For example (Input --> Output):
+
+// 39 --> 3 (because 3*9 = 27, 2*7 = 14, 1*4 = 4 and 4 has only one digit)
+// 999 --> 4 (because 9*9*9 = 729, 7*2*9 = 126, 1*2*6 = 12, and finally 1*2 = 2)
+// 4 --> 0 (because 4 is already a one-digit number)
+
+//My solution
+
+function persistence(num) {
+    let result = 0,
+        str = String(num)
+    while( str.length > 1){
+      str = String( [...str].reduce((prev, curr) => prev * curr)  )
+      result ++
+    }
+    return result
+  }
+    
+    //split the number into two 
+    //multiply both numbers
+    //write a condition after each seperation and multiplication? checking if it's single digit. 
+    //repeat until we have a single digit 
