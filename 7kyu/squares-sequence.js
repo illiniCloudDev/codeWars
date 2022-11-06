c6477f1138be04755232c629fb130ac7b70ce320
illiniCloudDev <107890959+illiniCloudDev@users.noreply.github.com>
2022-11-05 22:44:58 -0500

    7kyu kata. The exponents are messing with me. Need to loop at loop to understand fully.

---
+++ 7kyu/squares-sequence.js
@@ -0,0 +1,22 @@
+// Complete the function that returns an array of length n, starting with the given number x and the squares of the previous number. If n is negative or zero, return an empty array/list.
+
+// Examples
+// 2, 5  -->  [2, 4, 16, 256, 65536]
+// 3, 3  -->  [3, 9, 81]
+
+
+
+//My solution 
+function squares(x, n) {
+    let result = []
+    if( n == 1 ){
+      return [x]
+    }
+    for( let i = 0; i < n - 1; i++){
+      if( i == 0){
+        result.push(x)
+      }
+      result.push(x *= x)
+    }
+    return result
+  }
