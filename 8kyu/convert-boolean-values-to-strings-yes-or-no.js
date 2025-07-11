bc4003fba6e24275c49a060f416d9a9822266241
illiniCloudDev <107890959+illiniCloudDev@users.noreply.github.com>
2025-07-10 22:31:28 -0500

    simple 8kyu kata

---
+++ 8kyu/convert-boolean-values-to-strings-yes-or-no.js
@@ -0,0 +1,12 @@
+//coding challenge 
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+// Coding Solution
+
+function boolToWord( bool ){
+  if(bool){
+    return "Yes"
+  }else{
+    return "No"
+  }
+}
