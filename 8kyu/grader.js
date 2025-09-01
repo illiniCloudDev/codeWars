73115d9a7ab6927f6ed14d0aacadee8ad50b20cf
illiniCloudDev <107890959+illiniCloudDev@users.noreply.github.com>
2025-08-31 23:53:14 -0500

    nice conditionals!

---
+++ 8kyu/grader.js
@@ -0,0 +1,24 @@
+//coding challenge
+// Score	Grade
+// Anything greater than 1 or less than 0.6	"F"
+// 0.9 or greater	"A"
+// 0.8 or greater	"B"
+// 0.7 or greater	"C"
+// 0.6 or greater	"D"
+
+
+//solution 
+function grader(score) {
+  
+  if(score > 1 || score < 0.6){
+    return "F"
+  }else if( score == 0.9 || score > 0.9){
+    return "A"
+  }else if( score == 0.8 || score > 0.8){
+    return "B"    
+  }else if( score == 0.7 || score > 0.7){
+    return "C"    
+  }else if( score == 0.6 || score > 0.6){
+    return "D"    
+  }
+}
