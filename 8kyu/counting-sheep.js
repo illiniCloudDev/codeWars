230e46c3f583d26cd698141a18d2d06f281793c8
Charlie Caneda <canedacharlie@gmail.com>
2025-09-02 22:43:50 -0500

    nice and easy kata!

---
+++ 8kyu/counting-sheep.js
@@ -0,0 +1,32 @@
+//Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+
+// For example,
+
+// [true,  true,  true,  false,
+//   true,  true,  true,  true ,
+//   true,  false, true,  false,
+//   true,  false, false, true ,
+//   true,  true,  true,  true ,
+//   false, false, true,  true]
+// The correct answer would be 17.
+
+// Hint: Don't forget to check for bad values like null/undefined
+
+
+//solution
+function countSheeps(sheep) {
+
+    let result = 0
+    for(let i = 0; i < sheep.length; i++){
+      if(sheep[i] === true ){
+        result += 1
+      }
+    }
+    return result 
+}
+
+//alternative solution that is much easier!
+
+function countSheeps(sheep) {
+  return sheep.filter(Boolean).length;
+}
